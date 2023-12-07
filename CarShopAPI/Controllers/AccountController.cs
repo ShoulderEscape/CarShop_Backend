3417b342b48bded9db5cce4b8727461306b9b4ff
BADRUMBADRUM <49311368+BADRUMBADRUM@users.noreply.github.com>
2023-12-07 15:08:26 +0100

    added login funtion

--- CarShopAPI/Controllers/AccountController.cs
+++ CarShopAPI/Controllers/AccountController.cs
@@ -3,6 +3,10 @@ using CarShopAPI.Data.UserRepositories;
 using CarShopAPI.Dto;
 using Entites;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace CarShopAPI.Controllers
 {
@@ -14,16 +18,19 @@ namespace CarShopAPI.Controllers
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
         private readonly ILogger<AccountController> _logger;
+        public static User user = new User();
+        private readonly IConfiguration _config;
 
         public AccountController(IUserRepository userRepository,
-            IMapper mapper, ILogger<AccountController> logger)
+            IMapper mapper, ILogger<AccountController> logger, IConfiguration configuration)
         {
             _logger = logger;
             _mapper = mapper;
             _userRepository = userRepository;
+            _config = configuration;
         }
 
-        [HttpPost]
+        [HttpPost("register")]
         public async Task<ActionResult<User>> PostUser(RegistrationDto registerDto)
         {
             var usernameExists = await _userRepository.CheckUsername(registerDto.UserName);
@@ -32,14 +39,12 @@ namespace CarShopAPI.Controllers
                 return BadRequest("Username already in use.");
             }
 
-            var newUser = new User
-            {
-                UserName = registerDto.UserName,
-                Password = HashPassword(registerDto.Password),
-            };
+            user.UserName = registerDto.UserName;
+            user.Password = HashPassword(registerDto.Password);
+
             try
             {
-                var createdUser = await _userRepository.AddUser(newUser);
+                var createdUser = await _userRepository.AddUser(user);
 
                 if (createdUser == null)
                 {
@@ -55,11 +60,51 @@ namespace CarShopAPI.Controllers
             }
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> LoginUser(RegistrationDto registerDto)
+        {
+            var usernameExists = await _userRepository.CheckUsername(registerDto.UserName);
+            if (!usernameExists)
+            {
+                return BadRequest("Username not found.");
+            }
+
+            if(!BCrypt.Net.BCrypt.Verify(registerDto.Password, user.Password))
+            {
+                return BadRequest("Wrong password");
+            }
+
+            string token = CreateToken(user);
+
+            return Ok(token);
+        }
+
         private string HashPassword(string password)
         {
             return BCrypt.Net.BCrypt.HashPassword(password);
         }
 
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim> 
+            { 
+                new Claim(ClaimTypes.Name, user.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("Jwt:Token").Value!));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: credentials
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
 
     }
 }
