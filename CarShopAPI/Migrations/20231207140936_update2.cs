bbd50db70c254d62d6428142f7745733eae0d677
NiclasEdstrom <113895402+NiclasEdstrom@users.noreply.github.com>
2023-12-09 20:13:09 +0100

    Fix imagelink

---
+++ CarShopAPI/Migrations/20231207140936_update2.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarShopAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class update2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Imagelink",
+                table: "Cars",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Imagelink",
+                table: "Cars",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
--- Entites/Car.cs
+++ Entites/Car.cs
@@ -13,7 +13,7 @@
         public int ContactNumber { get; set; }
         public int Price { get; set; }
         public string? Description { get; set; }
-        public string Imagelink { get; set; }
+        public string? Imagelink { get; set; }
         public DateTime AuctionDateTime { get; set; }
 
     }
